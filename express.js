1c29c59335a224d65614527dccbb1cf36f032fb7
samblygen <samblygen@yahoo.com>
2024-09-05 13:45:47 -0400

    user update

--- express.js
+++ express.js
@@ -12,6 +12,30 @@ app.use(bodyParser.json());
 // Define the port
 const port = process.env.PORT || 3000;
 
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+let users = [
+  {id: 1, name: 'Eric' },
+  {id: 2, name: 'Mason'},
+  {id: 3, name: 'Zoe'},
+  {id: 4, name: 'Arlene'},
+];
+
+let posts = [
+  {id:1, userID: 1, content: 'Wah Gwann'},
+  {id:2, userID: 2, content: 'Welcome to Jamaica'},
+  {id:3, userID: 3, content: 'Lets have fun!'},
+  {id:4, userID: 4, content: 'Live, Love and be Thankful'},
+];
+
+let comments = [
+  {id:1, postID: 1, userID: 2, comment: "Have fun!"},
+  {id: 2, postID: 4, userID: 3, comment: "Lets go! "},
+  {id: 3, postID: 2, userID: 1, comment: "Thanks for sharing"},
+  {id: 4, postID: 3, userID: 4, comment: "Nice Post"}
+];
+
